e3cd6c8b4d39829b82c2fd5196ae08a3e756cc02
Melao12125 <MatheusForte12125@gmail.com>
2023-06-22 23:23:44 -0300

    Test

--- src/auto/direction.py
+++ src/auto/direction.py
@@ -1,7 +1,12 @@
 class Compass:
     def __init__(self):
+        self.direc = 1
         self.directions = {0: "O", 1: "N", 2: "L", 3: "S"}
-        self.current_direction = self.directions[1]
+        self.current_direction = self.direc
 
     def get_current_direction(self):
+        return self.current_direction
+
+    def turn_right(self):
+        self.direc = self.direc + 1
         return self.current_direction
--- tests/direcTest.py
+++ tests/direcTest.py
@@ -6,4 +6,10 @@ def test_current_direc():
     print(f'The current direction is {d1.current_direction}')
 
 
-test_current_direc()
+def test_steer():
+    d1 = Compass()
+    print(d1.current_direction)
+    d1.turn_right()
+    print(d1.current_direction)
+
+test_steer()
